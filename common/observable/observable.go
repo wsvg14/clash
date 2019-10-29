ee49905398d4d6a899813e83cb3c861b5fab1ba8
Dreamacro <305009791@qq.com>
2019-10-29 17:56:37 +0800

    Refactor with raw map

--- common/observable/observable.go
+++ common/observable/observable.go
@@ -6,70 +6,59 @@ import (
 )
 
 type Observable struct {
-	iterable     Iterable
-	listener     *sync.Map
-	done         bool
-	doneLock     sync.RWMutex
-	listenerLock sync.Mutex
+	iterable Iterable
+	listener map[Subscription]*Subscriber
+	mux      sync.Mutex
+	done     bool
 }
 
 func (o *Observable) process() {
 	for item := range o.iterable {
-		o.listenerLock.Lock()
-		o.listener.Range(func(key, value interface{}) bool {
-			elm := value.(*Subscriber)
-			elm.Emit(item)
-			return true
-		})
-		o.listenerLock.Unlock()
+		o.mux.Lock()
+		for _, sub := range o.listener {
+			sub.Emit(item)
+		}
+		o.mux.Unlock()
 	}
 	o.close()
 }
 
 func (o *Observable) close() {
-	o.doneLock.Lock()
-	o.done = true
-	o.doneLock.Unlock()
+	o.mux.Lock()
+	defer o.mux.Unlock()
 
-	o.listenerLock.Lock()
-	o.listener.Range(func(key, value interface{}) bool {
-		elm := value.(*Subscriber)
-		elm.Close()
-		return true
-	})
-	o.listenerLock.Unlock()
+	o.done = true
+	for _, sub := range o.listener {
+		sub.Close()
+	}
 }
 
 func (o *Observable) Subscribe() (Subscription, error) {
-	o.doneLock.RLock()
-	done := o.done
-	o.doneLock.RUnlock()
-	if done == true {
+	o.mux.Lock()
+	defer o.mux.Unlock()
+	if o.done {
 		return nil, errors.New("Observable is closed")
 	}
 	subscriber := newSubscriber()
-	o.listenerLock.Lock()
-	o.listener.Store(subscriber.Out(), subscriber)
-	o.listenerLock.Unlock()
+	o.listener[subscriber.Out()] = subscriber
 	return subscriber.Out(), nil
 }
 
 func (o *Observable) UnSubscribe(sub Subscription) {
-	elm, exist := o.listener.Load(sub)
+	o.mux.Lock()
+	defer o.mux.Unlock()
+	subscriber, exist := o.listener[sub]
 	if !exist {
 		return
 	}
-	subscriber := elm.(*Subscriber)
-	o.listenerLock.Lock()
-	o.listener.Delete(subscriber.Out())
-	o.listenerLock.Unlock()
+	delete(o.listener, sub)
 	subscriber.Close()
 }
 
 func NewObservable(any Iterable) *Observable {
 	observable := &Observable{
 		iterable: any,
-		listener: &sync.Map{},
+		listener: map[Subscription]*Subscriber{},
 	}
 	go observable.process()
 	return observable
